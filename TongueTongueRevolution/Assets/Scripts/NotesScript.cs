5dfa516d609acdff324d0be9dcd0547430f9ecfb
Udon <udoooon0727@gmail.com>
2017-10-29 08:08:34 +0900

    プレイ動画に顔認識追加

--- TongueTongueRevolution/Assets/Scripts/NotesScript.cs
+++ TongueTongueRevolution/Assets/Scripts/NotesScript.cs
@@ -15,7 +15,7 @@ public class NotesScript : MonoBehaviour {
 	void Update () {
 		this.transform.position += Vector3.down * 10 * Time.deltaTime;
 		if (this.transform.position.y < -5.0f) {
-			Debug.Log("false");
+			//Debug.Log("false");
 			Destroy (this.gameObject);
 		}
 	}
